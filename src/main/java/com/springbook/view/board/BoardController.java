ad1784886cec24ffe5328f0450a77acdd9bdb146
kduckk00 <kduckk00@gmail.com>
2021-02-17 22:34:48 +0900

    Update DAY04_ing

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -3,7 +3,6 @@ package com.springbook.view.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
